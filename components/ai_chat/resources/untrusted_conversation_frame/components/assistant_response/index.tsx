42b00f6560c0fdb5e0c42eb4b2a7f7986f7f5503
Darnell Andries <dandries@brave.com>
2026-01-27 19:57:22 -0800

    [AI Chat] Add chart creation ability to code execution tool

--- components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/index.tsx
+++ components/ai_chat/resources/untrusted_conversation_frame/components/assistant_response/index.tsx
@@ -13,6 +13,7 @@ import MarkdownRenderer from '../markdown_renderer'
 import ToolEvent from './tool_event'
 import WebSourcesEvent from './web_sources_event'
 import MemoryToolEvent from './memory_tool_event'
+import Chart from './chart'
 import styles from './style.module.scss'
 import {
   removeReasoning,
@@ -156,6 +157,7 @@ function AssistantEvent(
 
 export type AssistantResponseProps = BaseProps & {
   events: Mojom.ConversationEntryEvent[]
+  toolArtifacts?: Mojom.ToolArtifact[] | null
 }
 
 export default function AssistantResponse(props: AssistantResponseProps) {
@@ -193,13 +195,19 @@ export default function AssistantResponse(props: AssistantResponseProps) {
         />
       ))}
 
-      {!props.isEntryInProgress && (
-        <>
-          {sourcesEvent && <WebSourcesEvent sources={sourcesEvent.sources} />}
-          {searchQueriesEvent && (
-            <SearchSummary searchQueries={searchQueriesEvent.searchQueries} />
-          )}
-        </>
+      {!props.isEntryInProgress && sourcesEvent && (
+        <WebSourcesEvent sources={sourcesEvent.sources} />
+      )}
+      {props.toolArtifacts
+        ?.filter((artifact) => artifact.type === Mojom.LINE_CHART_ARTIFACT_TYPE)
+        .map((artifact, i) => (
+          <Chart
+            key={i}
+            artifact={artifact}
+          />
+        ))}
+      {!props.isEntryInProgress && searchQueriesEvent && (
+        <SearchSummary searchQueries={searchQueriesEvent.searchQueries} />
       )}
     </AssistantResponseContextProvider>
   )
