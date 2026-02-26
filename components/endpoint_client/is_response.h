b2d20c02a3df9e1d0527644a4bc3039b960c0d7f
Szilard Szaloki <szilardszaloki@gmail.com>
2026-02-25 18:35:20 -0700

    Comments.

--- components/endpoint_client/is_response.h
+++ components/endpoint_client/is_response.h
@@ -11,17 +11,27 @@
 
 namespace endpoint_client::detail {
 
+// Primary template: a type does not satisfy IsResponse unless
+// matched by one of the partial specializations below.
 template <typename...>
 inline constexpr bool kIsResponse = false;
 
+// Partial specialization: Response<T, E> satisfies IsResponse
+// for a given BodyType if both T and E model valid response bodies
+// for that BodyType.
 template <typename T, typename E, typename BodyType>
   requires(IsResponseBody<T, BodyType> && IsResponseBody<E, BodyType>)
 inline constexpr bool kIsResponse<Response<T, E>, BodyType> = true;
 
+// Partial specialization (single-argument form): a type T satisfies
+// IsResponse if it models either a JSON response
+// or a Protobuf response.
 template <typename T>
 inline constexpr bool kIsResponse<T> =
     kIsResponse<T, JSON> || kIsResponse<T, Protobuf>;
 
+// Concept: a type satisfies IsResponse if
+// its kIsResponse specialization evaluates to true.
 template <typename... Ts>
 concept IsResponse = kIsResponse<Ts...>;
 
--- components/endpoint_client/is_response_body.h
+++ components/endpoint_client/is_response_body.h
@@ -11,17 +11,27 @@
 
 namespace endpoint_client::detail {
 
+// Primary template: a type does not satisfy IsResponseBody unless
+// matched by one of the partial specializations below.
 template <typename...>
 inline constexpr bool kIsResponseBody = false;
 
+// Partial specialization: a type T satisfies IsResponseBody
+// for a given BodyType if BodyType declares T as a valid
+// response body via BodyType::kIsResponseBody<T>.
 template <typename T, typename BodyType>
 inline constexpr bool kIsResponseBody<T, BodyType> =
     BodyType::template kIsResponseBody<T>;
 
+// Partial specialization (single-argument form): a type T satisfies
+// IsResponseBody if it models either a JSON response body
+// or a Protobuf response body.
 template <typename T>
 inline constexpr bool kIsResponseBody<T> =
     kIsResponseBody<T, JSON> || kIsResponseBody<T, Protobuf>;
 
+// Concept: a type satisfies IsResponseBody if
+// its kIsResponseBody specialization evaluates to true.
 template <typename... Ts>
 concept IsResponseBody = kIsResponseBody<Ts...>;
 
